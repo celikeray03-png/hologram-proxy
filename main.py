7584599b39b5bac7bbf493ebc49d957fd6ad45cb
celikeray03-png <celikeray03@gmail.com>
2026-09-17 20:53:51 +0300

    main.py

--- main.py
+++ main.py
@@ -1,32 +1,38 @@
 from flask import Flask, jsonify
 import requests
+from datetime import datetime, timedelta
 
 app = Flask(__name__)
 
 LIGLER = [
-    "tur.1",             # Süper Lig
-    "uefa.europa",       # Avrupa Ligi
-    "uefa.champions",    # Şampiyonlar Ligi
-    "uefa.europa.conf",  # Konferans Ligi
-    "eng.1",             # Premier League
-    "esp.1",             # La Liga
-    "ita.1",             # Serie A
-    "ger.1",             # Bundesliga
-    "fra.1"              # Ligue 1
+    {"slug": "tur.1", "ad": "Süper Lig"},
+    {"slug": "uefa.europa", "ad": "UEFA Avrupa Ligi"},
+    {"slug": "uefa.champions", "ad": "Şampiyonlar Ligi"},
+    {"slug": "uefa.europa.conf", "ad": "Konferans Ligi"},
+    {"slug": "eng.1", "ad": "Premier League"},
+    {"slug": "esp.1", "ad": "La Liga"},
+    {"slug": "ita.1", "ad": "Serie A"},
+    {"slug": "ger.1", "ad": "Bundesliga"},
+    {"slug": "fra.1", "ad": "Ligue 1"}
 ]
 
 @app.route('/')
 def home():
-    return "Hologram Cube Proxy Aktif!"
+    return "Hologram Cube Dynamic API Aktif!"
 
 @app.route('/maclar')
 def maclar_cek():
+    # Bugün ve Yarının Tarihlerini Alıyoruz (YYYYMMDD)
+    bugun = datetime.now()
+    yarin = bugun + timedelta(days=1)
+    tarih_param = f"{bugun.strftime('%Y%m%d')}-{yarin.strftime('%Y%m%d')}"
+    
     tum_maclar = []
     
     for lig in LIGLER:
         try:
-            url = f"https://site.api.espn.com/apis/site/v2/sports/soccer/{lig}/scoreboard"
-            res = requests.get(url, timeout=5)
+            url = f"https://site.api.espn.com/apis/site/v2/sports/soccer/{lig['slug']}/scoreboard?dates={tarih_param}"
+            res = requests.get(url, timeout=4)
             if res.status_code != 200:
                 continue
             
@@ -37,7 +43,6 @@ def maclar_cek():
                 comp = event['competitions'][0]
                 status = event['status']
                 
-                # Takım isimleri (Short Name yoksa Full Name)
                 ev = comp['competitors'][0]['team'].get('shortDisplayName') or comp['competitors'][0]['team'].get('name', 'EV')
                 dep = comp['competitors'][1]['team'].get('shortDisplayName') or comp['competitors'][1]['team'].get('name', 'DEP')
                 
@@ -47,12 +52,23 @@ def maclar_cek():
                 state = status['type']['state']
                 d = status['type']['shortDetail']
                 
-                # Durum Ayarı
+                # Tarih/Saat Formatı
+                date_str = event.get('date', '')
+                gun_etiketi = ""
+                if 'T' in date_str:
+                    mac_tarihi = date_str.split('T')[0].replace('-', '')
+                    saat = date_str.split('T')[1][:5]
+                    # Türkiye Saati Ötelemesi (Yaklaşık +3)
+                    saat_int = (int(saat.split(':')[0]) + 3) % 24
+                    saat_fmt = f"{saat_int:02d}:{saat.split(':')[1]}"
+                    
+                    if mac_tarihi == bugun.strftime('%Y%m%d'):
+                        gun_etiketi = f"Bugün {saat_fmt}"
+                    else:
+                        gun_etiketi = f"Yarın {saat_fmt}"
+                
                 if state == "pre":
-                    # UTC saatini basitçe alıyoruz
-                    date_str = event.get('date', '')
-                    if 'T' in date_str:
-                        d = date_str.split('T')[1][:5]
+                    d = gun_etiketi
                 elif state == "in":
                     d = f"{d} CANLI"
                 elif state == "post" or d in ["FT", "FINAL"]:
@@ -60,13 +76,14 @@ def maclar_cek():
                 
                 tum_maclar.append({
                     "id": str(event['id']),
+                    "lig": lig['ad'],
                     "ev": ev.upper()[:9],
                     "dep": dep.upper()[:9],
                     "evS": ev_skor,
                     "depS": dep_skor,
                     "dk": d
                 })
-        except Exception as e:
+        except Exception:
             continue
             
     return jsonify({"maclar": tum_maclar})
